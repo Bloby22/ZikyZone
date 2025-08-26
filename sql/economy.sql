7d879124e47e03719c2d213765383b07d3a53636
BlobyCZ <blobycz@proton.me>
2025-08-26 08:29:16 +0200

    Delete sql directory

--- sql/economy.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS economy (
-  user_id TEXT PRIMARY KEY,
-  zcoins INTEGER DEFAULT 0,
-  daily_streak INTEGER DEFAULT 0,
-  weekly_streak INTEGER DEFAULT 0,
-  last_daily DATE,
-  last_weekly INTEGER
-);
